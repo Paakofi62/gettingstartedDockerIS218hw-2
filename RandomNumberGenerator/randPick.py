bbe6b76244e2e4a38b6c2df4322c81f3b5d5d759
Paakofi72 <koo7@njit.edu>
2020-02-29 10:32:49 -0500

    Feature: added random generator functions

---
+++ RandomNumberGenerator/randPick.py
@@ -0,0 +1,23 @@
+import random
+
+class RandPick:
+    @staticmethod
+    def randPick(data):
+        return random.choice(data)
+
+    @staticmethod
+    def randPickSeed(seed, data):
+        random.seed(seed)
+        return RandPick.randPick(data)
+
+    @staticmethod
+    def randPickList(nums, data):
+        dlist = []
+        while len(dlist) < nums:
+            dlist.append(RandPick.randPick(data))
+        return dlist
+
+    @staticmethod
+    def ranPickListSeed(seed, nums, data):
+        random.seed(seed)
+        return RandPick.randPickList(nums, data)
---
+++ RandomNumberGenerator/randomList.py
@@ -0,0 +1,11 @@
+from RandomNumberGenerator.randomNum import RandomNum
+
+
+class RandomList:
+
+    @staticmethod
+    def randomList(length, seed, x, y):
+        data = []
+        while len(data) != length:
+            data.append(RandomNum.randomNumSeed(seed, x, y))
+        return data
